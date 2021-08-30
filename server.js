7a678c026c9cb252716bc7f09ebfe89dc8abc2e8
Amro Abbas <37993690+amro-kamal@users.noreply.github.com>
2021-08-30 17:16:50 +0000

    remove comments

--- server.js
+++ server.js
@@ -46,11 +46,7 @@ mongoose
     console.log("connected to DataBase ,plant-app-database");
   })
   .catch((err) => console.log("mongoose ,catch database error", err)); // mongoose.connect('mongodb://localhost:27017/PlantsApp');
-// let gfs;
-// connect.once("open", () => {
-//   console.log("connected to DataBase ,plant-app-database");
-//   gfs = new mongoose.mongo.GridFSBucket(connect.db, { bucketName: "uploads" });
-// });
+
 /////////////////////////////////////////////
 
 console.log(__dirname);
@@ -87,16 +83,7 @@ app.get("/api/disease", (req, res) => {
 });
 app.post("/api/disease", (req, res) => {
   const disease = req.body;
-  // const disease = {
-  //   disease_id: "gebr113",
-  //   title: "Esca Black rot",
-  //   category:"disease",
-  //   plant:'Grape',
-  //   summary:"Grape black rot is a fungal disease caused by an ascomycetous fungus, Guignardia bidwellii, that attacks grape vines during hot and humid weather.  It can cause complete crop loss in warm, humid climates, but is virtually unknown in regions with arid summers. The name comes from the black fringe that borders growing brown patches on the leaves. The disease also attacks other parts of the plant,all green parts of the vine: the shoots, leaf and fruit stems, tendrils, and fruit.",
-  //   symptoms:"Relatively small, brown circular lesions develop on infected leaves and within a few days tiny black spherical fruiting bodies (pycnidia) protrude from them. Elongated black lesions on the petiole may eventually girdle these organs, causing the affected leaves to wilt. Shoot infection results in large black elliptical lesions. These lesions may contribute to breakage of shoots by wind, or in severe cases, may girdle and kill young shoots altogether. This fungus bides its time. Most plants show very little signs of infection until its too late. They will look very healthy until fruit sets. Even flowering will be normal",
-  //   treatment:"The use of chemical control is widely available for agricultural purposes. To apply chemical applications, look at the fungicide label for proper use. Be sure that the conditions are optimal to spray to avoid drift and inefficiencies of the fungicide due to application. Fungicide guidelines must be followed. There are a wide variety of chemicals that are available for both regular and organic growers. Commercially, application of fungicides may be costly. To cut down on costs, one must understand the life cycle of the pathogens. Different fungicides are more effective at certain infection stages.",
-  //   imageUrl:"/diseases/black_rot.jpg"
-  // };
+
   console.log("adding disease ", disease);
   Disease.create(disease, (err, doc) => {
     if (err) {
@@ -135,92 +122,8 @@ app.post("/api/history", async (req, res) => {
     const history = user.history;
     console.log("user history ", history);
 
-    // open the mongodb connection with the connection
-    // string stored in the variable called url.
-    // MongoClient.connect(
-    //   DBuri,
-    //   {
-    //     useNewUrlParser: true,
-    //     useUnifiedTopology: true,
-    //   },
-    //   function (err, db) {
-    //     var dbo = db.db("plant-app-database");
-    //     console.log("history mongoooooooooooooooooooo");
-    //     // perform a mongodb search and return only one result.
-    //     // convert the variabvle called filename into a valid
-    //     // objectId.
-
-    //     var i;
-    //     var filename = [];
-    //     for (i = 0; i < history.length; i++) {
-    //       console.log("i= ", i);
-    //       // await history.forEach((history_item) => {
-
-    //       ///////////////////////////////////////////
-    //       try {
-    //         dbo
-    //           .collection("images")
-    //           .findOne({ filename: history[i].pic }, function (err, result) {
-    //             if (err) console.log("images.find error ", err);
-    //             else {
-    //               // set the http response header so the browser knows this
-    //               // is an 'image/jpeg' or 'image/png'
-    //               // res.setHeader("content-type", result.contentType);
-    //               // send only the base64 string stored in the img object
-    //               // buffer element
-    //               // res.send(result.img.buffer);
-    //               console.log("findone i=", i);
-    //               console.log("images.find ", result);
-    //               history[i].pic = result;
-    //             }
-    //           });
-    //       } catch {}
-
-    //       /////////////////////////////////////////
-    //       // });
-    //     }
-    //   }
-    // );
-
-    //   (async () => {
-    //     let client = await MongoClient.connect(DBuri, {
-    //       useNewUrlParser: true,
-    //       useUnifiedTopology: true,
-    //     });
-
-    //     let db = client.db("plant-app-database");
-    //     console.log("history mongoooooooooooooooooooo");
-
-    //     var i;
-    //     try {
-    //       for (i = 0; i < history.length; i++) {
-    //         console.log("i= ", i);
-    //         const image = await db
-    //           .collection("images")
-    //           .findOne({ filename: history[i].pic });
-
-    //         // console.log(`res => ${JSON.stringify(res)}`);
-    //         console.log("findone i=", i);
-    //         console.log("images.find ", image);
-    //         // imgcopy = { ...image };
-    //         // imgcopy.img.buffer = imgcopy.img.buffer.toString;
-    //         history[i].pic = image;
-    //       }
-    //       console.log("modified history");
-    //       return res.json({ history: history });
-    //     } finally {
-    //       client.close();
-    //     }
-    //   })().catch((err) => console.error(err));
-    // });
-
     (async () => {
-      // let client = await MongoClient.connect(DBuri, {
-      //   useNewUrlParser: true,
-      //   useUnifiedTopology: true,
-      // });
 
-      // let db = client.db("plant-app-database");
       console.log("history mongoooooooooooooooooooo");
 
       var i;
@@ -244,30 +147,6 @@ app.post("/api/history", async (req, res) => {
     })().catch((err) => console.error(err));
   });
 
-  // HistoryItem.find()
-  //   .sort({ _id: order })
-  //   .exec((err, doc) => {
-  //     if (err) return res.status(400).send(err);
-  //     res.send(doc);
-  //   });
-
-  /*const disease = {
-    disease_id: "gebr113",
-    title: "Esca Black rot",
-    category:"disease",
-    hosts:"Grape",
-    summary:"Grape black rot is a fungal disease caused by an ascomycetous fungus, Guignardia bidwellii, that attacks grape vines during hot and humid weather.  It can cause complete crop loss in warm, humid climates, but is virtually unknown in regions with arid summers. The name comes from the black fringe that borders growing brown patches on the leaves. The disease also attacks other parts of the plant,all green parts of the vine: the shoots, leaf and fruit stems, tendrils, and fruit.",
-    symptoms:"Relatively small, brown circular lesions develop on infected leaves and within a few days tiny black spherical fruiting bodies (pycnidia) protrude from them. Elongated black lesions on the petiole may eventually girdle these organs, causing the affected leaves to wilt. Shoot infection results in large black elliptical lesions. These lesions may contribute to breakage of shoots by wind, or in severe cases, may girdle and kill young shoots altogether. This fungus bides its time. Most plants show very little signs of infection until its too late. They will look very healthy until fruit sets. Even flowering will be normal",
-    treatment:"The use of chemical control is widely available for agricultural purposes. To apply chemical applications, look at the fungicide label for proper use. Be sure that the conditions are optimal to spray to avoid drift and inefficiencies of the fungicide due to application. Fungicide guidelines must be followed. There are a wide variety of chemicals that are available for both regular and organic growers. Commercially, application of fungicides may be costly. To cut down on costs, one must understand the life cycle of the pathogens. Different fungicides are more effective at certain infection stages.",
-    imageUrl:"/diseases/black_rot.jpg"
-  };*/
-  // HistoryItem.create(historyItem, (err, doc) => {
-  //   if (err) return res.status(400).send(err);
-  //   res.send({
-  //     success: true,
-  //     historyItem: doc,
-  //   });
-  // });
 });
 
 app.get("/api/history", (req, res) => {
@@ -296,12 +175,6 @@ app.get("/api/history", (req, res) => {
         dbo
           .collection("images")
           .findOne({ filename: filename }, function (err, result) {
-            // set the http response header so the browser knows this
-            // is an 'image/jpeg' or 'image/png'
-            // res.setHeader("content-type", result.contentType);
-            // send only the base64 string stored in the img object
-            // buffer element
-            // res.send(result.img.buffer);
             history_item.pic = result;
           });
       }
@@ -309,84 +182,14 @@ app.get("/api/history", (req, res) => {
   });
 
   res.json({ history: history });
-  // HistoryItem.find()
-  //   .sort({ _id: order })
-  //   .exec((err, doc) => {
-  //     if (err) return res.status(400).send(err);
-  //     res.send(doc);
-  //   });
+
 });
 
 // POST //
 // const upload = require("./server/multer/uploadImage");
 const uploadFile = require("./server/multer/uploadImageToDB");
 
-// app.post(
-//   "/api/classify",
-//   [mongouploadController.uploadFile, upload.single("imageData")],
-//   async (req, res) => {
-//     console.log("start classifying req=", req.file);
-//     if (req.file) {
-//       console.log(
-//         "image was sent , inside image block ,filename=",
-//         req.file.filename
-//       );
-//       // const pic = {
-//       //   imageName: req.file.filename,
-//       //   // imageData: req.file.path
-//       //   imageData: "uploads/" + req.file.filename,
-//       // };
-//       const pic = "";
-//       const path = req.file.path;
 
-//       let predictionResult = await startPrediction(path).catch((e) =>
-//         console.log("startprediction catch", e)
-//       );
-//       console.log("prediction reuslt", predictionResult);
-
-//       res.json({
-//         success: true,
-//         result: predictionResult,
-//         imagePath: path, //important
-//         relativePath: "uploads/" + req.file.filename, // relative path to fetch local images
-//       });
-//       console.log("res was sent", {
-//         success: true,
-//         result: predictionResult,
-//         imagePath: path, //important
-//         relativePath: "uploads/" + req.file.filename, // relative path to fetch local images
-//       });
-
-//       const data = {
-//         title: predictionResult.diseaseId, //from classification
-//         date: Date.now(),
-//         disease_id: predictionResult.diseaseId, //from classification
-//         confidence: predictionResult.confidence, //from classification
-//         pic,
-//       };
-
-//       const user = await User.findOne({ email: "amro@gmail.com" });
-//       const history = user.history;
-//       // console.log("user history ", history);
-//       if (history.length < 10) {
-//         history_update = { history: [...history, data] };
-//       } else {
-//         history.shift();
-//         history.push(data);
-//         history_update = { history: history };
-//       }
-//       await user.updateOne(history_update, () => {
-//         console.log("history updated");
-//       });
-//     } else {
-//       console.log("no image  sent , inside no image block");
-//       res.status(400).send({
-//         success: false,
-//         err: "no image sent/file not valid",
-//       });
-//     }
-//   }
-// );
 MongoClient = require("mongodb").MongoClient;
 fs = require("fs-extra");
 app.post("/api/classify", upload.single("imageData"), async (req, res) => {
@@ -419,15 +222,7 @@ app.post("/api/classify", upload.single("imageData"), async (req, res) => {
         } else {
           console.log("image uploaded to db");
         }
-        // var newoid = new ObjectId(result.ops[0]._id);
-        // fs.remove(req.file.path, function (err) {
-        //   if (err) {
-        //     console.log(err);
-        //   }
-        //   console.log("image was uploaded");
 
-        //   res.render("index", { title: "Thanks for the Picture!" });
-        // });
       });
     }
   );
@@ -438,11 +233,7 @@ app.post("/api/classify", upload.single("imageData"), async (req, res) => {
       "image was sent , inside image block ,filename=",
       req.file.filename
     );
-    // const pic = {
-    //   imageName: req.file.filename,
-    //   // imageData: req.file.path
-    //   imageData: "uploads/" + req.file.filename,
-    // };
+
     const pic = req.file.filename;
     const path = req.file.path;
 
@@ -493,32 +284,7 @@ app.post("/api/classify", upload.single("imageData"), async (req, res) => {
   }
 });
 
-// app.get("/api/image", (req, res) => {
-//   console.log("image");
-//   // Image.find({}, function (err, results) {
-//   //   if (err) {
-//   //     console.log("error retreiving the image", err);
-//   //     res.send("error retreiving the image");
-//   //   }
-//   //   if (res == null) {
-//   //   } else {
-//   //     // set the http response header so the browser knows this
-//   //     // is an 'image/jpeg' or 'image/png'
-//   //     res.setHeader("content-type", results.contentType);
-//   //     // send only the base64 string stored in the img object
-//   //     // buffer element
-//   //     res.send(results.img.buffer);
-//   //   }
-//   // });
-//   gfs
-//     .find({ filename: "1597534504341-bezkoder-image (3).JPG" })
-//     .toArray((err, files) => {
-//       if (!files[0] || files.length === 0) {
-//         return res.status(200).json({ success: false, message: "no files" });
-//       }
-//       res.status(200).json({ success: true, file: files[0] });
-//     });
-// });
+
 
 app.get("/api/image", function (req, res) {
   console.log("get image ", req.body.filename);
@@ -566,26 +332,7 @@ app.post("/api/register", (req, res) => {
         res.status(404).json({ message: "This email is already registered" });
       } else {
         createnewuser(user, date);
-        // //name
-        // User.find({ name: user.name }).then((doc) => {
-        //   if (doc.length > 0) {
-        //     console.log("name found", doc);
-        //     //password
-        //     User.find({ password: user.password }).then((doc) => {
-        //       if (doc.length > 0) {
-        //         console.log(
-        //           "password also found=>These user name and password are already used by someone else"
-        //         );
-        //         res.status(400).json({
-        //           message:
-        //             "These user name and password are already used by someone else",
-        //         });
-        //       } else {
-        //         createnewuser(user);
-        //       }
-        //     });
-        //   } else createnewuser(user);
-        // });
+
       }
     })
     .catch((e) => {
@@ -693,54 +440,4 @@ const getDate = () => {
   return today;
 };
 
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// // const cookieParser = require("cookie-parser");
-// const mongoose = require("mongoose");
-// const config = require("./server/config/config").get(process.env.NODE_ENV);
-// const cors = require("cors");
 
-// const { google } = require("googleapis");
-// const multer = require("multer");
-// const multerDrive = require("multer-drive");
-// const app = express();
-// app.use(cors());
-// app.use(bodyParser.json());
-
-// const auth = new google.auth.JWT({
-//   email: "plantapp@plantapp-1597518888765.iam.gserviceaccount.com",
-//   key: "103295581831170496051",
-//   scopes: ["https://www.googleapis.com/auth/drive"],
-// });
-// const upload = multer({
-//   storage: multerDrive(auth),
-//   // Rest of multer's options
-// });
-
-// /*
-//         Skip to the part where you define your routes
-//     */
-// const router = express.Router();
-
-// app.post("/upload", (req, res) => {
-//   console.log("uploading...........", req.body.file);
-//   if (!req.file) {
-//     return res.status(500).send("An error occurred while uploading your file");
-//   }
-//   console.log("sending to drive");
-//   res.status(200).send("Hurray! File was uploaded.");
-// });
-
-// app.post("/uploadMany", upload.array("photos", 6), (req, res) => {
-//   if (!req.files || !req.files.length) {
-//     return res.status(500).send("An error occurred while uploading your files");
-//   }
-//   res.status(200).send("Hurray! Files were uploaded.");
-// });
-
-// const port = process.env.PORT || 4000;
-// console.log("running on port", port);
-
-// app.listen(port, () => {
-//   console.log(`SERVER RUNNNING`);
-// });
